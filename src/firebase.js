6d79abfff59fdee64c804629148e93b53db67d22
A1doranM <vaselisk1997@gmail.com>
2022-04-24 15:50:06 +0300

    configured firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDKBaXzvchVcv-Tztu1rDsxjNT-slMES5s",
@@ -9,3 +10,11 @@ const firebaseConfig = {
     appId: "1:771787583189:web:851e9fdc22f5bbc95a7c5c",
     measurementId: "G-6RVF5C87DH"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider;
+
+export {auth, provider};
+export default db;
