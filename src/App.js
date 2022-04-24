be9fb4e4105f7ce129438f619b739e127fa3802e
A1doranM <vaselisk1997@gmail.com>
2022-04-24 12:45:20 +0300

    improved css, added variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import Messenger from "./components/Messanger/Messenger";
 import Sidebar from "./components/Sidebar/Sidebar";
 import React from "react";
-import "./App.css";
 
 function App() {
     return (
-        <div className="App">
+        <>
             <Sidebar/>
             <main>
                 <Messenger/>
             </main>
-        </div>
+        </>
     );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Sidebar.css";
 import SearchIcon from '@mui/icons-material/Search';
+import {BorderColorOutlined} from "@mui/icons-material";
+import {IconButton} from "@mui/material";
 
 function Sidebar(props) {
     return (
@@ -10,6 +12,9 @@ function Sidebar(props) {
                     <SearchIcon className="sidebar__searchIcon"/>
                     <input type="text" placeholder="Search" className="sidebar__input"/>
                 </div>
+                <IconButton variant="outlined" id="sidebar__button">
+                    <BorderColorOutlined/>
+                </IconButton>
             </div>
             <div className="sidebar__threads"></div>
             <div className="sidebar__bottom"></div>
